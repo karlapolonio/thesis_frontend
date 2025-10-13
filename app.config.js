ecbd3cf1570e6c4e91e5292134c00dd5b8ab158a
karl-apolonio <kahootpurpose3@gmail.com>
2025-10-13 15:55:13 +0800

    chore: add eas, updates and runtime version  in app.config.js

--- app.config.js
+++ app.config.js
@@ -1,11 +1,22 @@
 import 'dotenv/config';
 
-export default ({ config }) => {
-  return {
-    ...config,
+export default {
+  expo: {
+    name: "thesis_frontend",
+    slug: "thesis_frontend",
+    version: "1.0.0",
     extra: {
       API_KEY: process.env.API_KEY,
       BACKEND_URL: process.env.BACKEND_URL,
+      eas: {
+        projectId: "a391dfff-70c7-4fe7-8bf4-a2b31ef9a33a"
+      }
     },
-  };
+    updates: {
+      url: "https://u.expo.dev/a391dfff-70c7-4fe7-8bf4-a2b31ef9a33a"
+    },
+    runtimeVersion: {
+      policy: "appVersion"
+    }
+  }
 };
